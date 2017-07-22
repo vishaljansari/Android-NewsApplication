9d612f79e4b573442c4ef33f068eed1fc1bff577
VISHAL JANSARI <vishaljansari@hotmail.com>
2017-07-22 00:29:24 -0700

    corrected url to image column in DB

--- app/src/main/java/com/example/vishal/newsapp/Database/Contract.java
+++ app/src/main/java/com/example/vishal/newsapp/Database/Contract.java
@@ -16,7 +16,7 @@ public class Contract {
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_DESCRIPTION = "description";
         public static final String COLUMN_URL = "url";
-        public static final String COLUMN_URL_TO_IMAGE = "publishedAt";
+        public static final String COLUMN_URL_TO_IMAGE = "urlToImage";
         public static final String COLUMN_PUBLISHED_AT = "publishedAt";
 
     }
